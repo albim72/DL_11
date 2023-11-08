e26a51b729374b7d5e51364bd2b45795613f1f20
albim72 <marcin.albiniak72@gmail.com>
2023-11-08 11:42:11 +0100

    Update pojazd.py

--- DZIEN_1/TRANSPORT/pojazd.py
+++ DZIEN_1/TRANSPORT/pojazd.py
@@ -7,7 +7,21 @@ class Pojazd(STransportu):
         self.nrtrasy = nrtrasy
         self.czas_przejazdu = czas_przejazdu
         self.koszt_paliwa = koszt_paliwa
-        self.inne_koszty = inne_koszty
+        self._inne_koszty = inne_koszty
+
+    def __repr__(self):
+        return (f'nowy obiekt klasy {self.__class__.__name__}: środek transportu -> {self.nazwa}'
+                f', numer trasy: {self.nrtrasy} , czas przejazdu: {self.czas_przejazdu} min')
+
+    #funkcja getter
+    @property
+    def inne_koszty(self):
+        return self._inne_koszty
+
+    #funkcja setter
+    @inne_koszty.setter
+    def inne_koszty(self,nowekoszty):
+        self._inne_koszty = nowekoszty
 
     def danest(self):
         return f'Pojazd -> {self.rodzaj}, nazwa pojazdu: {self.nazwa}.'
